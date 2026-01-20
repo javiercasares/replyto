4adfd3368873c623c3381f33360456c9fbe66f21
Javier Casares <javier@casares.org>
2026-01-20 17:52:01 +0100

    update things

--- replyto.php
+++ replyto.php
@@ -5,10 +5,10 @@
  * Requires at least: 4.1
  * Requires PHP: 5.6
  * Version: 1.3.0
- * Author: Javier Casares
- * Author URI: https://www.javiercasares.com/
- * License: GPL-2.0-or-later
- * License URI: https://spdx.org/licenses/GPL-2.0-or-later.html
+ * Author: ROBOTSTXT
+ * Author URI: https://www.robotstxt.es/
+ * License: GPL-3.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain: replyto
  * Domain Path: /languages
  *
